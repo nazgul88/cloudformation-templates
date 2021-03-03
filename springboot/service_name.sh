822ddd1bad80f5365c3fabc628794bc8e382bc31
nazgul88 <nshorukova@gmail.com>
2021-03-02 19:43:14 -0600

    adding systemd files

---
+++ springboot/service_name.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+SERVICE_NAME=pegasus
+PATH_TO_JAR=/var/lib/jenkins/workspace/pet-clinic/target/spring-petclinic-2.3.1.BUILD-SNAPSHOT.jar
+PID_PATH_NAME=/tmp/pegasus.pid
+case $1 in
+start)
+       echo "Starting $SERVICE_NAME ..."
+  if [ ! -f $PID_PATH_NAME ]; then
+       nohup java -jar $PATH_TO_JAR --server.port=8081 /tmp 2>> /dev/null >>/dev/null &
+                   echo $! > $PID_PATH_NAME
+       echo "$SERVICE_NAME started ..."
+  else
+       echo "$SERVICE_NAME is already running ..."
+  fi
+;;
+stop)
+  if [ -f $PID_PATH_NAME ]; then
+         PID=$(cat $PID_PATH_NAME);
+         echo "$SERVICE_NAME stoping ..."
+         kill $PID;
+         echo "$SERVICE_NAME stopped ..."
+         rm $PID_PATH_NAME
+  else
+         echo "$SERVICE_NAME is not running ..."
+  fi
+;;
+restart)
+  if [ -f $PID_PATH_NAME ]; then
+      PID=$(cat $PID_PATH_NAME);
+      echo "$SERVICE_NAME stopping ...";
+      kill $PID;
+      echo "$SERVICE_NAME stopped ...";
+      rm $PID_PATH_NAME
+      echo "$SERVICE_NAME starting ..."
+      nohup java -jar $PATH_TO_JAR --server.port=8081 /tmp 2>> /dev/null >> /dev/null &
+      echo $! > $PID_PATH_NAME
+      echo "$SERVICE_NAME started ..."
+  else
+      echo "$SERVICE_NAME is not running ..."
+     fi     ;;
